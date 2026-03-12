14a84e3d18fa5e0a18e00e01cf51b3b38d0223cc
Yashwant-GH <yashwantkaushik2003@gmail.com>
2026-03-12 18:39:45 +0530

    implement dock presentation

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/Welcome.jsx";
+import Dock from "#components/Dock.jsx";
 
-export { Navbar, Welcome };
+export { Navbar, Welcome, Dock};
